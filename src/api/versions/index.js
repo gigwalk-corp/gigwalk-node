5ee9859b089415b0d228f48a976fdc42d88572c6
Steven Bassett <steven.j.bassett@gmail.com>
2016-07-28 13:29:19 -0700

    Update: add basic requests

--- src/api/versions/index.js
+++ src/api/versions/index.js
@@ -2,8 +2,26 @@
 import Resource from '../Resource';
 import type { $AxiosXHR, $AxiosXHRConfigBase } from 'axios';
 
+export type APIRes<T> = {
+  _meta: Object,
+  warnings: mixed,
+  gw_api_response: Array<Object>,
+  _metadata: Object,
+  code: number,
+  data: T,
+  errors: mixed
+};
+
+export type VersionsData = [{
+    version: string,
+    minimum_android_version: string,
+    minimum_ios_version: string
+}];
+
+export type APIPromise<T> = Promise<$AxiosXHR<APIRes<T>>>;
+
 export default class Versions extends Resource {
-    get(config?: $AxiosXHRConfigBase<*>): Promise<$AxiosXHR<Object>> {
+    get(config?: $AxiosXHRConfigBase<*>): APIPromise<VersionsData> {
         return this.client.get('/v1/versions/current', config);
     }
 }
